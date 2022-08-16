ce54c885bc497136fc6811d560b6746b58ccfaec
mjr2595 <michaeljohnraymond@gmail.com>
2022-08-15 19:48:00 -0500

    primero

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# install xcode build tools
+xcode-select --install
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install homebrew formulae
+
+# install homebrew casks
+
+# setup zsh with Starship
+
+# configure git
+git config --global user.name mjr2595
+git config --global user.email michaeljohnraymond@gmail.com
+git config --global init.defaultbranch main
+
+# create dev folder
+mkdir ~/Dev
