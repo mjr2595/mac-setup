24865601f118cf1150ff95b6b3721966e6042beb
mjr2595 <michaeljohnraymond@gmail.com>
2022-09-24 03:00:04 -0500

    update

--- setup.sh
+++ setup.sh
@@ -15,5 +15,18 @@ git config --global user.name mjr2595
 git config --global user.email ****
 git config --global init.defaultbranch main
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install vim-plug
+
+# Vim (~/.vim/autoload)
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Neovim (~/.local/share/nvim/site/autoload)
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # create dev folder
 mkdir ~/Dev
