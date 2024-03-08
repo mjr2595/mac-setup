d1d93eb801060c66ed64f76ba70e24631ebf98a1
mjr2595 <michaeljohnraymond@gmail.com>
2024-03-08 11:03:25 -0600

    python stuff added to setup and included iterm config

--- setup.sh
+++ setup.sh
@@ -8,7 +8,16 @@ xcode-select --install
 
 # install homebrew casks
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# omz zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
 # setup zsh with Starship
+brew install starship
+mkdir ~/.confg/starship.toml
+# copy over starship.toml
 
 # disable last login terminal prompt
 touch .hushlogin
@@ -18,9 +27,19 @@ git config --global user.name mjr2595
 git config --global user.email ****
 git config --global init.defaultbranch main
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# create dev folder
+mkdir ~/Dev
 
+# Python stuff
+brew update
+brew install pyenv
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc # should already be in there
+pyenv install -l
+pyenv install [PYTHON_VERSION]
+pyenv global [PYTHON_VERSION]
+pyenv versions
+
+# OPTIONAL: Vim stuff
 # install vim-plug
 
 # Vim (~/.vim/autoload)
@@ -30,6 +49,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # Neovim (~/.local/share/nvim/site/autoload)
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# create dev folder
-mkdir ~/Dev
