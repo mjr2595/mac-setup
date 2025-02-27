4387c4696a2272c4ea551eaf565e356ce245f3b1
mjr2595 <michaeljohnraymond@gmail.com>
2025-02-26 19:54:51 -0600

    Update Brewfiles and setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ xcode-select --install
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# install homebrew stuff, might need to tweak this file first
-brew bundle install --file=./apps/Brewfile
-
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # omz zsh-autosuggestions
