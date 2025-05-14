416f8df4402d2691fb317409c13f1fa6d010b4ee
mjr2595 <michaeljohnraymond@gmail.com>
2025-05-13 22:23:03 -0500

    reformat plist_to_json

--- utils/plist-to-json/plist_to_json.py
+++ utils/plist-to-json/plist_to_json.py
@@ -1,27 +1,27 @@
-import plistlib
 import json
+import plistlib
 
 # Load the .plist file
-with open('text_replacements.plist', 'rb') as f:
+with open("text_replacements.plist", "rb") as f:
     plist_data = plistlib.load(f)
 
 # Transform the .plist data to the desired JSON format
 json_data = []
 for item in plist_data:
-    if item.get('shortcut').startswith(":"):
-        name = item.get('shortcut').split(":")[1]
+    if item.get("shortcut").startswith(":"):
+        name = item.get("shortcut").split(":")[1]
     else:
-        name = item.get('phrase').split()[0]
+        name = item.get("phrase").split()[0]
     entry = {
         "name": name,
-        "text": item['phrase'],
+        "text": item["phrase"],
     }
-    if 'shortcut' in item:
-        entry["keyword"] = item['shortcut']
+    if "shortcut" in item:
+        entry["keyword"] = item["shortcut"]
     json_data.append(entry)
 
 # Save the JSON data to a file
-with open('text_replacements.json', 'w', encoding="utf-8") as f:
+with open("text_replacements.json", "w", encoding="utf-8") as f:
     json.dump(json_data, f, ensure_ascii=False, indent=2)
 
 print("Conversion complete. JSON file saved as 'text_replacements.json'")
